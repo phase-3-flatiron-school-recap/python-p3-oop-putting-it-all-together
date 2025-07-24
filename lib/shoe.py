e0815697a821299da5fe717624d288ce4ad73188
David Muchahi <Muchahi853@gmail.com>
2025-07-24 20:14:05 +0700

    Finished lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand="Adidas",size=9):
+        self.brand=brand
+        self.size=size
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self,size):
+        if isinstance(size,int):
+            self._size=size
+        else:
+            print("size must be an integer")
+    
+
+    def cobble(self):
+        self.condition="New"
+        print("Your shoe is as good as new!")
+
+
+    
+
+    
